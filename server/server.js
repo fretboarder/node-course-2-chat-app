800c1892189627fa80502aab99c2783ce4d7c1bc
fretboarder <speedskating@ymail.com>
2018-11-10 11:46:39 +0100

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,11 +16,22 @@ const io = socketIO(server)
 io.on('connection', (socket) => {
     console.log('new client connected')
     
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    })
+    
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: `New user joined`,
+        createdAt: new Date().getTime()
+    })
   
     socket.on('createMessage', (message) => {
         console.log('createMessage', message)
-        // emit to every connection!!!
-        io.emit('newMessage', {
+        
+        socket.broadcast.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
@@ -31,8 +42,6 @@ io.on('connection', (socket) => {
         console.log('client disconnected')
     })
 })
-
-
 
 
 app.use(express.static(publicPath))
